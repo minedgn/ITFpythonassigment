3875c9c1b3dccf8172a26d510db4d97d0a4c4e26
Mine Dogan <doganmn25@gmail.com>
2022-05-11 23:10:58 +0200

    new-document

---
+++ fibonaccinumber.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Create a list consisting of Fibonacci numbers from 1 to 55 using control flow statements and range() function.
+
+def fib(n):
+
+    if n > 1:
+
+        return fib(n-1) + fib(n-2)
+
+    return n
+
+for i in range(11):
+
+    print(fib(i))    
+    
+    
+    
+    
+    
+
